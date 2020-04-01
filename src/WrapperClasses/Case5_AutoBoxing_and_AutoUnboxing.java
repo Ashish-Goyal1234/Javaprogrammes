73dc7ec7b75cb1f763ae3f118472d3a6ac7abf64
unknown <ashishg120@gmail.com>
2020-04-01 10:17:27 +0530

    Video 5, 6 programmes

---
+++ src/WrapperClasses/Case5_AutoBoxing_and_AutoUnboxing.java
@@ -0,0 +1,32 @@
+package WrapperClasses;
+
+import java.util.ArrayList;
+
+public class Case5_AutoBoxing_and_AutoUnboxing {
+
+    public static void main(String[] args) {
+        //Autoboxing : Automatic conversion of primitive to wrapper object.
+        Integer i = 100;
+        System.out.println(i);
+        
+        //Auto Unboxing : Automatic conversion of wrapper object to primitive.
+        
+        int x = new Integer(200);
+        System.out.println(x);
+        
+        // Java 1.4 : no Autoboxing
+        ArrayList a1 = new ArrayList();
+        a1.add(new Integer(10));
+        a1.add(new Double(10.5));
+        a1.add(new Character('c'));
+        System.out.println(a1);
+        
+        // Java 1.5 : with AutoBoxing
+        ArrayList a = new ArrayList();
+        a.add(10);   //internally uses valueOf(). 
+        a.add(10.5);
+        a.add('c');
+        System.out.println(a);
+    }
+
+}
---
+++ src/WrapperClasses/Case6_Equal_And_DoubleEqulaopera_Wrapperclasses.java
@@ -0,0 +1,36 @@
+package WrapperClasses;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Comparison;
+
+public class Case6_Equal_And_DoubleEqulaopera_Wrapperclasses {
+
+    public static void main(String[] args) {
+        Integer i1 = new Integer(10);
+        Integer i2 = new Integer(10);
+        System.out.println(i1==i2);     //false
+        System.out.println(i1.equals(i2)); //true
+        
+        Integer x1 = new Integer(200);
+        Integer x2 = new Integer(200);
+        System.out.println(x1==x2);   //fals
+        System.out.println(x1.equals(x2));  //true
+    
+    //In case of valueOf method will always cache value in the range -128 to 127
+    
+        Integer j1 = Integer.valueOf(10);
+        Integer j2 = Integer.valueOf(10);
+        System.out.println(j1==j2);                     //true
+        System.out.println(j1.equals(j2));      //true
+        
+        Integer y1 = Integer.valueOf(200);
+        Integer y2 = Integer.valueOf(200);
+        System.out.println(y1==y2);                     //false
+        System.out.println(y1.equals(y2));              //true
+    
+   /*     Note : equals ()  always checks data Comparison.class
+        new operator : == creates different objects.
+        
+        valueOf() :  -128  to 127 : ref variable are pointing to same object.*/
+    }
+
+}
