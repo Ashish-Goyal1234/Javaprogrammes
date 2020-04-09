a6068bdf8aa6b72b83fd4788d4a83cf91cd23b09
Ashish Goyal <ashishg120@gmail.com>
2020-04-09 22:40:03 +0530

    Added Pattern Programmes

---
+++ PatternProgrammes/Programmes/Case4.java
@@ -0,0 +1,22 @@
+/*
+                  *  *  *  * 
+                  *  *  * 
+                  *  * 
+                  * 
+ */
+
+package Programmes;
+
+
+public class Case4 {
+
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+}
