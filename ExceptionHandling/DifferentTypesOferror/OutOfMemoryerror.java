d69dc7027323987023ba892e7369602e992698c2
Ashish goyal <ashishg120@gmail.com>
2020-04-12 02:10:05 -0700

    Completed Exception handling

---
+++ ExceptionHandling/DifferentTypesOferror/OutOfMemoryerror.java
@@ -0,0 +1,8 @@
+package DifferentTypesOferror;
+
+
+public class OutOfMemoryerror {
+    public static void main(String[] args) {
+    int a[] = new int[1000000000];
+    }
+}
---
+++ ExceptionHandling/DifferentTypesOferror/StackOverFlowError.java
@@ -0,0 +1,17 @@
+package DifferentTypesOferror;
+
+
+public class StackOverFlowError {
+
+    void m1(){
+        m2();
+    }
+    void m2(){
+        m1();
+    }
+    public static void main(String[] args) {
+    
+            new StackOverFlowError().m1();
+    }
+
+}
