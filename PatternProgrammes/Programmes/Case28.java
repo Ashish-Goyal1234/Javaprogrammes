3f8825db92c25f85821d75d67f3cfa347f7be949
Ashish goyal <ashishg120@gmail.com>
2020-05-31 17:42:29 +0530

    Hacker rank and Pattern programmes

---
+++ PatternProgrammes/Programmes/Case28.java
@@ -0,0 +1,27 @@
+/*
+           1
+         12
+       123
+     1234
+
+ */
+
+
+package Programmes;
+
+
+public class Case28 {
+
+    public static void main(String[] args) {
+        for(int i =1;i<=4;i++){
+            for(int j = 4-1;j>=i;j--){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ PatternProgrammes/Programmes/Case4.java
@@ -0,0 +1,22 @@
+/*
+                  *  *  *  * 
+                  *  *  * 
+                  *  * 
+                  * 
+ */
+
+package Programmes;
+
+
+public class Case4 {
+
+    public static void main(String[] args) {
+            for(int i=1;i<=5;i++){
+                for(int j=5;j>=i;j--){
+                    System.out.print(" * ");
+                }
+                System.out.println();
+            }
+            
+    }
+}
