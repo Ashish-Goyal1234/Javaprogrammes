9210ffa8fd9565cfdef9cbcf18c8ad49358cd4cc
unknown <ashishg120@gmail.com>
2020-11-16 09:48:45 +0530

    Replace words programme

---
+++ Collections/LinkedList/InterviewQuestion.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class InterviewQuestion {
+
+    public static void main(String[] args) {
+        List<String> list1 = new LinkedList<String>();
+        list1.add("sam");
+        list1.add("Tom");
+        list1.addLast(100);
+        list1.add("Jack");
+        list1.addFirst(20);
+        System.out.println("List1");
+    }
+
+}
