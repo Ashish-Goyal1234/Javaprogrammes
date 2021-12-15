e33370de69a2696210009076afec76d2d7f71ef3
Ashish <ashishg120@gmail.com>
2021-12-15 12:49:54 +0530

    Committing programmes

--- Collections/MethodsInCollectionListAndSet/HashMapMethods.java
+++ Collections/MethodsInCollectionListAndSet/HashMapMethods.java
@@ -6,7 +6,7 @@ public class HashMapMethods {
 
     public static void main(String[] args) {
             HashMap<String, Integer> hm = new HashMap<String, Integer>();
-            hm.ge
+  
     }
 
 }
